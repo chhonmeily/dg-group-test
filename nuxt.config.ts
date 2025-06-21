05874f4cbd9cc046c40f4186ca4f5164fd6020ea
kevinslash <kevin@slash.co>
2025-06-21 19:09:05 +0700

    menu wip but done horizontal in desktop

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/app.css"],
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/scripts', '@nuxt/image']
+})
